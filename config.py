fa166301eb897170d800dd9356178c2782effb3d
修远 <1099919785@qq.com>
2026-04-20 00:51:42 +0800

    chore: 更新训练轮数和早停耐心值

--- config.py
+++ config.py
@@ -30,8 +30,8 @@ NUM_LAYERS = 2              # 隐藏层数量
 DROPOUT = 0.3               # 丢弃率
 LEARNING_RATE = 0.001       # 学习率
 BATCH_SIZE = 1024             # 批处理大小
-EPOCHS = 25                 # 训练轮数
-PATIENCE = 20              # 早停耐心值
+EPOCHS = 200                 # 训练轮数
+PATIENCE = 150              # 早停耐心值
 
 # ==================== 数据存储路径 ====================
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
